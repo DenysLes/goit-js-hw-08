7b7f5146548f352e956f992f8f09c7a91d1d480a
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-02-07 18:16:33 +0200

    DONE commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,30 +1,37 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
 
 // Описаний в документації
-// import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Change code below this line
-const gallery = document.querySelector('.gallery');
-const markup = createMurkup(galleryItems);
 
-function createMurkup(galleryItems) {
-    return galleryItems.map(({ preview, original, description }) => {
-        return `<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
+const galleryContainer = document.querySelector('.gallery');
+const galleryCardsSet = createGallery(galleryItems);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item" >
+  <a class="gallery__item" href="${original}" >
   <img class="gallery__image" src="${preview}" alt="${description}"/>
 </a>
-</li>`
-    }).join('');
+</div>`;
+    })
+    .join('');
 }
-  
-gallery.insertAdjacentHTML('beforeend', markup);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryCardsSet);
 
 const lightbox = new SimpleLightbox('.gallery a', {
   caption: true,
   captionsData: 'alt',
   captionDelay: 250,
 });
+
