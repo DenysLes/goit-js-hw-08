b91bb4557fb8ccb10b1e63e3176536b3da495046
DenysLes <157131711+DenysLes@users.noreply.github.com>
2025-02-07 17:02:15 +0200

    unknown commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+// // Описаний в документації
+// import SimpleLightbox from "simplelightbox";
+
+// // Додатковий імпорт стилів
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const markup = createMurkup(galleryItems);
 
-console.log(galleryItems);
+function createMurkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>
+</li>`
+    }).join('');
+}
+  
+gallery.insertAdjacentHTML('beforeend', markup);
 
-
+const lightbox = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
